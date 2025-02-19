58072e6f8496c1816661bdfc312c7778c21ceb64
Noe Kurnianto <estehmanis450@gmail.com>
2025-02-19 19:53:05 +0700

    Create noe.py

---
+++ team/noe.py
@@ -0,0 +1,10 @@
+def perkenalan(nama: str, nim: str, peran: str) -> None:
+    print(f"Halo! Perkenalkan, nama saya {nama}")
+    print(f"NIM : {nim}")
+    print(f"Peran saya yaitu : {peran}")
+
+if __name__ == "__main__":
+    nama = "Noe Uriel Kurnianto" 
+    nim = "2213010369"
+    peran = "repository maker"
+    perkenalan(nama, nim, peran)
