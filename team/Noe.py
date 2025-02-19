2b317727398f7b0e465cdc84c2d4ed51e58b2d5f
oktavc <oktaviona2004@gmail.com>
2025-02-19 12:15:30 +0700

    Add files via upload

---
+++ team/Noe.py
@@ -0,0 +1,10 @@
+def perkenalan(nama: str, nim: str, peran: str) -> None:
+    print(f"Halo! Perkenalkan, nama saya {nama}")
+    print(f"NIM : {nim}")
+    print(f"Peran saya yaitu : {peran}")
+
+if __name__ == "__main__":
+    nama = "Noe Uriel kurnianto" 
+    nim = "2213010369"
+    peran = "Membuat Repositori"
+    perkenalan(nama, nim, peran)
